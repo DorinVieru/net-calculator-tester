454332124baa5e37c2e3874cc94af0f42ab77929
DorinVieru <dorino.vieru@gmail.com>
2024-05-09 17:40:35 +0200

    UP: Complete tests for every function; complete exercise

--- net-calculator-test/UnitTest1.cs
+++ net-calculator-test/UnitTest1.cs
@@ -1,21 +1,78 @@
 using NUnit.Framework;
 using net_calculator_test;
+using net_calculator_tester;
 
 namespace net_calculator_test
 {
     public class Tests
     {
-        //[SetUp]
-        //public void Setup()
-        //{
-        //}
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-        //[Test]
-        //public void Test1()
-        //{
-        //    Assert.Pass();
-        //}
+        // TEST ADD
+        [Test]
+        public void TestAdd()
+        {
+            try
+            {
+                float expected = 13;
+                float actual = Calculator.Add(5, 8);
+                Assert.AreEqual(expected, actual, "Addizione fallita. Ricontrolla.");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Il test TestAdd per la funzione Add è fallito: {ex.Message}");
+            }
+        }
 
+        // TEST SUBSTRACT
+        [Test]
+        public void TestSubtract()
+        {
+            try
+            {
+                float expected = 5;
+                float actual = Calculator.Subtract(10, 5);
+                Assert.AreEqual(expected, actual, "Sottrazione fallita. Ricontrolla.");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Il test StesSubstract per la funzione Substract è fallito: {ex.Message}");
+            }
+        }
 
+        // TEST DIVIDE
+        [Test]
+        public void TestDivide()
+        {
+            try
+            {
+                float expected = 4;
+                float actual = Calculator.Divide(40, 10);
+                Assert.AreEqual(expected, actual, "Divisione fallita. Ricontrolla.");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Il test TestDivide per la funzione Divide è fallito: {ex.Message}");
+            }
+        }
+
+        // SEST MULTIPLY
+        [Test]
+        public void TestMultiply()
+        {
+            try
+            {
+                float expected = 30;
+                float actual = Calculator.Multiply(10, 3);
+                Assert.AreEqual(expected, actual, "Moltiplicazione fallita. Ricontrolla.");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Il test TestMultiply per la funzione Multiply è fallito: {ex.Message}");
+            }
+        }
     }
 }
