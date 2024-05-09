77acac16a775b846f4c0e4466dbe492666545d0e
DorinVieru <dorino.vieru@gmail.com>
2024-05-09 15:08:02 +0200

    UP: Create new project for test

---
+++ net-calculator-test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using net_calculator_test;
+
+namespace net_calculator_test
+{
+    public class Tests
+    {
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
+
+        //[Test]
+        //public void Test1()
+        //{
+        //    Assert.Pass();
+        //}
+
+
+    }
+}
