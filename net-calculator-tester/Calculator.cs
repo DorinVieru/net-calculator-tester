e44845fe4d6ba41803578b1290ce0249808750cb
DorinVieru <dorino.vieru@gmail.com>
2024-05-09 15:07:34 +0200

    UP: Write function for add, substract, divide and moltiply

---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public class Calculator
+    {
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public float Divide(float num1, float num2)
+        {
+            if (num2 == 0)
+                throw new DivideByZeroException("Non è possibile dividere per zero.");
+            return num1 / num2;
+        }
+
+        public float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
